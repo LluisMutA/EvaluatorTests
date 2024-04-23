18f532456e89f3942022e103b4d4790a730a33a3
LluisMutA <lluismut@gmail.com>
2024-04-23 16:20:57 +0200

    EvaluatorTest 1

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Evaluator {
 
@@ -5,16 +8,70 @@ public class Evaluator {
     public static int calculate(String expr) {
         // Convertim l'string d'entrada en una llista de tokens
         Token[] tokens = Token.getTokens(expr);
-        // Efectua el procediment per convertir la llista de tokens en notació RPN
-        // Finalment, crida a calcRPN amb la nova llista de tokens i torna el resultat
-        return 0;
+        Token[] tokenRPN = RPN(tokens); // Efectua el procediment per convertir la llista de tokens en notació RPN
+        return calcRPN(tokenRPN);// Finalment, crida a calcRPN amb la nova llista de tokens i torna el resultat
+    }
+
+    public static Token[] RPN(Token[] RPN){
+        List<Token> transformaRPN = new ArrayList<>();
+        Stack<Token> pilaRPN = new Stack<>();
+         for (Token token: RPN){
+             if(token.getTtype() == Token.Toktype.NUMBER){
+                 transformaRPN.add(token);
+             }else if (token.getTtype() == Token.Toktype.OP) {
+                        if((!pilaRPN.isEmpty()) && (prioritat(pilaRPN.peek()) >= prioritat(token))){ //compr mentre
+                            transformaRPN.add(pilaRPN.pop());
+                        }
+                        pilaRPN.push(token);
+                 }
+             }
+        return RPN;
+    }
+
+    public static int prioritat(Token otk){
+        char c = otk.getTk();
+        if (c == '+' || c == '-'){
+            return 1;
+        }
+        if (c == '*' || c == '/'){
+            return 2;
+        }
+        if (c == '^') {
+            return 3;
+        }return 0;
     }
 
     public static int calcRPN(Token[] list) {
         // Calcula el valor resultant d'avaluar la llista de tokens
-        return 0;
-        
+        Stack<Integer> stack = new Stack<>();
+
+        for (Token token : list) {
+            if (token.getTtype() == Token.Toktype.NUMBER) {
+                stack.push(token.getValue());
+            } else if (token.getTtype() == Token.Toktype.OP) {
+                int b = stack.pop();
+                int a = stack.pop();
+                char oper = token.getTk();
+                int result = operador(a, b, oper);
+                stack.push(result);
+            }
+        }return stack.pop();
+}
+
+
+    private static int operador(int a, int b, char oper) {
+        switch (oper) {
+            case '+':
+                return a+b;
+            case '-':
+                return a-b;
+            case '/':
+                return a/b;
+            case '*':
+                return a*b;
+            case '^':
+                return (int) Math.pow(a, b);
+        }
+        return oper;
     }
-
-
 }
